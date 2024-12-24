d7f3e51ce9b8e975fb48853ff8e6a2e6216494f2
Caio Carvalho <caiocarvalho.py@gmail.com>
2024-12-24 09:38:46 -0400

    add user authentication views and templates with dashboard and sign-up functionality

--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
-# Create your views here.
+
+def dashboard(request):
+    return render(request, "users/dashboard.html")
+
+
+def sign_up(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(reverse("dashboard"))
+    else:
+        form = UserCreationForm()
+    return render(request, "registration/sign_up.html", {"form": form})
